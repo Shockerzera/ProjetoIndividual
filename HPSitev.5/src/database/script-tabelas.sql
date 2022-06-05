5f62925685fe8dc0e832f1271b0373a7abe7400c
Shockerzera <miguel.schrempp@sptech.school>
2022-06-05 19:14:42 -0300

    Gráfico de pessoas

--- HPSitev.5/src/database/script-tabelas.sql
+++ HPSitev.5/src/database/script-tabelas.sql
@@ -6,12 +6,11 @@ CREATE TABLE casa(
     nomeCasa VARCHAR(64)
 );
 
-
 CREATE TABLE cliente(
     idCliente INT PRIMARY KEY AUTO_INCREMENT,
     nomeCliente VARCHAR(64),
-    emailCliente varchar(64),
-    senha varchar(64),
+    emailCliente varchar(45),
+    senha varchar(45),
     fkCasa INT,
     FOREIGN KEY (fkCasa) REFERENCES casa (idCasa)
 );
@@ -36,13 +35,15 @@ SELECT * FROM quiz;
 insert into quiz values (1, 0, 1); -- Insert dos pontos do cliente
 insert into quiz values (2, 0, 2); -- Insert dos pontos do cliente
 insert into quiz values (3, 0, 3); -- Insert dos pontos do cliente
-insert into quiz values (4, 0, 4); -- Insert dos pontos do cliente
+insert into quiz values (4, 0, 4); -- Insert dos pontos do cliente	
 
 select count(fkCasa) as 'Quantidade de Pessoas na Grifinória' from cliente where fkCasa = 1;
 select count(fkCasa) as 'Quantidade de Pessoas na Sonserina' from cliente where fkCasa = 2;
 select count(fkCasa) as 'Quantidade de Pessoas na Lufa-Lufa' from cliente where fkCasa = 3;
 select count(fkCasa) as 'Quantidade de Pessoas na Corvinal' from cliente where fkCasa = 4;
 
+select count(fkCasa)as 'Quantidade_de_Pessoas', nomeCasa from cliente inner join casa on fkCasa = idCasa group by fkCasa;
+
 SELECT SUM(pontos) as 'Pontos', nomeCasa FROM quiz JOIN cliente ON fkCliente = idCliente JOIN casa ON fkCasa = idCasa GROUP BY nomeCasa;
 
-truncate table quiz; -- Limpar dados dos quizes
+truncate table quiz;
